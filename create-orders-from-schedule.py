3c0435d9695d8c5ac04f2ca2424fc7ef5682f329
Kevin Hill <kevinkhill@gmail.com>
2018-08-10 21:24:14 +0100

    adding error checking and better console output to order creation

--- create-orders-from-schedule.py
+++ create-orders-from-schedule.py
@@ -12,11 +12,18 @@ for router in scheduled_work:
         router['gid'] = gid_map[router['part_number']]
 
         # print(repr(router))
-        print('| Found GID[{gid}] for {part_number}'.format(**router))
-        print('+--> Creating order for RT#{order_number} - {description}'.format(**router))
+        print('|', flush=True)
+        print('| Found GID[{gid}] for {part_number}'.format(**router), flush=True)
+        print('+--> Creating order for RT#{order_number} - {description}'.format(**router), flush=True)
 
         response = fastems.create_order(router)
-        result = utils.parse_response(response)
+        result = utils.parse_response(response)['CreateOrderResponse']['CreateOrderResult']
 
-        print('|---->', result)
-        sleep(1000)
+        if result['Success'] == 'False':
+            msg = '|----> ' + result['Message']['Text']
+            msg = msg.format(router['part_number'], router['order_number'])
+        else:
+            msg = '|----> Success!'
+
+        print(msg, flush=True)
+        sleep(.5)
